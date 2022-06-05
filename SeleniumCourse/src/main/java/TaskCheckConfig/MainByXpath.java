0c7a3fc565eccff132645270031691e6763a42be
Adrian <adrian.bialobrodzki@gmail.com>
2022-06-05 15:58:48 +0200

    Solved task 4 in Selenium: MainByXpath

--- SeleniumCourse/src/main/java/TaskCheckConfig/MainByXpath.java
+++ SeleniumCourse/src/main/java/TaskCheckConfig/MainByXpath.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class MainByClassName {
-    public static void main(String[] args) {
+public class MainByXpath {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -16,6 +16,13 @@ public class MainByClassName {
         WebElement accountInput = driver.findElement(By.className("account_input"));
         accountInput.sendKeys(generateEmail()); //generowanie adresu
         driver.findElement(By.id("SubmitCreate")).click();
+
+        Thread.sleep(2000); //dodać wyjątek do sygnatury metody
+        driver.findElement(By.xpath("//input[@id='customer_firstname']")).sendKeys("John");
+        driver.findElement(By.xpath("//input[@name='customer_lastname']")).sendKeys("Rambo");
+        WebElement emailInput = driver.findElement(By.xpath("//input[@id='email']"));
+        driver.findElement(By.xpath("//input[@name='passwd']")).sendKeys("StrongPass");
+        driver.findElement(By.xpath("//button[@id='submitAccount']")).click();
         driver.quit();
     }
 
